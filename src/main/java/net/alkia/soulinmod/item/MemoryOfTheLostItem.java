b09c61dd76cb8e6d117aa2e6a16a4b4317721016
Payzza <Jacob@DESKTOP-UA7IRV0>
2020-07-05 21:31:34 -0400

    yeetus

--- src/main/java/net/alkia/soulinmod/item/MemoryOfTheLostItem.java
+++ src/main/java/net/alkia/soulinmod/item/MemoryOfTheLostItem.java
@@ -1,12 +1,27 @@
 
 package net.alkia.soulinmod.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.alkia.soulinmod.itemgroup.SoulintaryMusicItemGroup;
+import net.alkia.soulinmod.SoulinmodModElements;
+
+import java.util.List;
+
 @SoulinmodModElements.ModElement.Tag
 public class MemoryOfTheLostItem extends SoulinmodModElements.ModElement {
-
 	@ObjectHolder("soulinmod:memory_of_the_lost")
 	public static final Item block = null;
-
 	public MemoryOfTheLostItem(SoulinmodModElements instance) {
 		super(instance, 18);
 	}
@@ -15,9 +30,7 @@ public class MemoryOfTheLostItem extends SoulinmodModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new MusicDiscItemCustom());
 	}
-
 	public static class MusicDiscItemCustom extends MusicDiscItem {
-
 		public MusicDiscItemCustom() {
 			super(0, SoulinmodModElements.sounds.get(new ResourceLocation("soulinmod:memoryofthelost")),
 					new Item.Properties().group(SoulintaryMusicItemGroup.tab).maxStackSize(1).rarity(Rarity.RARE));
@@ -30,7 +43,5 @@ public class MemoryOfTheLostItem extends SoulinmodModElements.ModElement {
 			list.add(new StringTextComponent("??5A music disc that plays a"));
 			list.add(new StringTextComponent("??5sombre melody."));
 		}
-
 	}
-
 }
